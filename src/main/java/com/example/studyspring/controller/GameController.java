988174b89a464079bbda81139390f1c333108b84
Anh Nguyen Tung <anhntz1@nec.vn>
2024-03-29 11:40:09 +0700

    commit READ and CREATE

--- src/main/java/com/example/studyspring/controller/GameController.java
+++ src/main/java/com/example/studyspring/controller/GameController.java
@@ -5,10 +5,7 @@ import com.example.studyspring.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,7 @@ public class GameController {
     }
 
     @PostMapping("/add")
+    @ResponseBody
     public GameList addGame(@RequestBody GameList gameList){
         return gameService.addGame(gameList);
     }
